4bf650332da04749d97544e5369f31c822d2107b
BEAUHAUS <beau.html@gmail.com>
2019-05-12 02:35:07 -0400

    adds results, API rendering images, etc

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,25 +1,47 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./UseDropdown";
-
-// console.log("anim[2]", ANIMALS[2]);
-// console.log("animals", ANIMALS);
+import Results from "./Results";
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  /* NOTE:
+   * This async function
+   *
+   *
+   */
+  async function requestPets() {
+    console.log("async function requestPets ran");
+    const { animals } = await pet.animals({
+      pet,
+      breed,
+      type: animal
+    });
+    console.log("animals: ", animals);
+
+    setPets(animals || []);
+  }
 
   /* NOTE:
    * useEffect will be "scheduled to" run AFTER
    * 1st render of SearchParms return value
+   * _____
+   * [animal, setBreed, setBreeds] are "dependencies" of sorts
+   * if any of them change,(say, animal), useEffect re-renders
+   * and calls the API anew.
+   * IN ORDER TO RUN IT ONCE & NO MORE -> use an empty array
+   * IN ORDER TO RUN IT EVERY SINGLE TIME COMP RENDERS, remove the param entirely
    */
   useEffect(() => {
     setBreeds([]);
     setBreed("");
-    pet.breeds(animal).then(({ breeds }) => {
-      const breedStrings = breeds.map(({ name }) => name);
+    pet.breeds(animal).then(({ breeds: APIbreeds }) => {
+      const breedStrings = APIbreeds.map(({ name }) => name);
       setBreeds(breedStrings);
     }, console.error);
   }, [animal, setBreed, setBreeds]);
@@ -27,7 +49,14 @@ const SearchParams = () => {
   return (
     <div className="search-params">
       <h1>{location}</h1>
-      <form action="" className="search-form">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+          console.log("submit");
+        }}
+        className="search-form"
+      >
         <label htmlFor="location">
           location
           <input
@@ -44,6 +73,7 @@ const SearchParams = () => {
 
         <button type="submit">Submit</button>
       </form>
+      <Results pets={pets} />;
     </div>
   );
 };
